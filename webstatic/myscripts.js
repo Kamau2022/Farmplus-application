08f13814b2206a342acfa64af19c544ec6eb5fdd
kamau2022 <benkabenson@gmail.com>
2023-03-15 12:52:43 +0300

    First commit

---
+++ webstatic/myscripts.js
@@ -0,0 +1,8 @@
+$("#button1").click(function() {  
+  $("#bux form").toggle("slow");
+  return false;
+});
+$("#button").click(function() {  
+  $("#buxas form").toggle("slow");
+  return false;
+});
