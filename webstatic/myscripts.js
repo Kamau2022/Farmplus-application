859bb988b19d1dd57eed76e470aaf642f12b7cb8
kamau2022 <benkabenson@gmail.com>
2023-03-30 11:21:49 +0300

    changes were made successfully

--- webstatic/myscripts.js
+++
@@ -1,8 +0,0 @@
-$("#button1").click(function() {  
-  $("#bux form").toggle("slow");
-  return false;
-});
-$("#button").click(function() {  
-  $("#buxas form").toggle("slow");
-  return false;
-});
